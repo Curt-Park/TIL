e51fe028f593d5f78ab7f3d0cedb10fc15f86c7a
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2019-06-25 00:34:56 +0900

    Add reverse_words.py

--- Readme.md
+++ Readme.md
@@ -5,6 +5,7 @@
 |:---:|-----------------------|----------|------|--------|------|
 | 0   | Merging meeting times | array | [Interview Cake](https://www.interviewcake.com/question/python3/merging-ranges?course=fc1&section=array-and-string-manipulation) | [A](array/merge_meeting_times.py) | + |
 | 1   | Reverse array in place| array | [Interview Cake](https://www.interviewcake.com/question/python3/reverse-string-in-place?course=fc1&section=array-and-string-manipulation) | [A](array/reverse_array.py) | + |
+| 2   | Reverse words | array | [Interview Cake](https://www.interviewcake.com/question/python3/reverse-words?course=fc1&section=array-and-string-manipulation) | [A](array/reverse_words.py) | - |
 
 
 +: Solved within 30 min
---
+++ array/reverse_words.py
@@ -0,0 +1,68 @@
+"""
+https://www.interviewcake.com/question/python3/reverse-words?course=fc1&section=array-and-string-manipulation
+
+Your team is scrambling to decipher a recent message, 
+worried it's a plot to break into a major European National Cake Vault. 
+The message has been mostly deciphered, but all the words are backward! 
+Your colleagues have handed off the last step to you.
+
+Write a function reverse_words() that takes a message as a list of characters 
+and reverses the order of the words in place. 
+
+For example:
+
+  message = [ 'c', 'a', 'k', 'e', ' ',
+            'p', 'o', 'u', 'n', 'd', ' ',
+            's', 't', 'e', 'a', 'l' ]
+
+reverse_words(message)
+
+# Prints: 'steal pound cake'
+print(''.join(message))
+
+When writing your function, assume the message contains only letters and spaces,
+and all words are separated by one space.
+"""
+
+
+def reverse_array(arr, i, j):
+    n = j - i + 1
+
+    for k in range(n // 2):
+        left, right = i + k, i + n - k - 1
+        arr[left], arr[right] = arr[right], arr[left]
+
+
+def reverse_words(arr):
+    n = len(arr)
+
+    if n <= 1:
+        return arr
+
+    reverse_array(arr, 0, n - 1)
+
+    i = 0
+    for j in range(n):
+        if arr[j] == ' ':
+            reverse_array(arr, i, j - 1)
+            i = j + 1
+
+        if j + 1 == n:
+            reverse_array(arr, i, n - 1)
+
+
+arr1 = []
+arr2 = ['a']
+arr3 = [
+    'c', 'a', 'k', 'e', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 's', 't', 'e', 'a',
+    'l'
+]
+
+reverse_words(arr1)
+reverse_words(arr2)
+reverse_words(arr3)
+
+assert '' == ''.join(arr1)
+assert 'a' == ''.join(arr2)
+assert 'steal pound cake' == ''.join(arr3)
+
