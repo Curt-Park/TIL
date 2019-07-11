130f58036d9b115a072616b993cd9caea70bde57
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2019-07-12 01:19:05 +0900

    Add rotate_matrix.py

--- array/rotate_matrix.py
+++ array/rotate_matrix.py
@@ -7,7 +7,7 @@ from typing import List
 
 
 def solution(mat: List[List[int]]) -> List[List[int]]:
-    """
+    """ space complexity: O(1)
     >>> solution([[]])
     [[]]
     >>> solution([[2]])
@@ -22,21 +22,46 @@ def solution(mat: List[List[int]]) -> List[List[int]]:
     if n <= 1:
         return mat
 
+    offset = 0
+    for i in range(n, 1, -2):  # 6 -> 4 -> 2 or 7 -> 5 -> 3
+        for j in range(i - 1):
+            tmp_top = mat[offset][offset+j]
+            # left to top
+            mat[offset][offset+j] = mat[i-1-j+offset][offset]
+            # bottom to left
+            mat[i-1-j+offset][offset] = mat[i-1+offset][i-1-j+offset]
+            # right to bottom
+            mat[i-1+offset][i-j-1+offset] = mat[j+offset][i-1+offset]
+            # top to right
+            mat[j+offset][i-1+offset] = tmp_top
+
+        offset += 1
+
+    return mat
+
+
+def solution2(mat: List[List[int]]) -> List[List[int]]:
+    """ space complexity: O(N^2)
+    >>> solution2([[]])
+    [[]]
+    >>> solution2([[2]])
+    [[2]]
+    >>> solution2([[1, 2], [3, 4]])
+    [[3, 1], [4, 2]]
+    >>> solution2([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+    """
+
+    n = len(mat)
+    if n <= 1:
+        return mat
+
     tmp_mat = deepcopy(mat)
     for i in range(n):
         for j in range(n):
             mat[j][n-i-1] = tmp_mat[i][j]
 
     return mat
-
-"""
-00 01 02.  20 10 00
-10 11 12.  21 11 01
-20 21 22.  22 12 02
-
-00 01. 10 00
-10 11. 11 01
-"""
 
 
 if __name__ == "__main__":
