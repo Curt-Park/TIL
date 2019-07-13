317257328ec7ddac8c27f46bdcecd03f3880c05f
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2019-07-13 13:10:40 +0900

    Add string rotation

--- array/set_matrix_zeros.py
+++ array/set_matrix_zeros.py
@@ -42,13 +42,66 @@ A simple improvement uses O(m + n) space, but still not the best solution.
 Could you devise a constant space solution?
 """
 
+from typing import List
+
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
+        >>> s = Solution()
+        >>> s.setZeroes([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        [[1, 0, 1], [0, 0, 0], [1, 0, 1]]
+        >>> s.setZeroes([[0, 1, 2 ,0], [3, 4 ,5, 2], [1, 3, 1, 5]])
+        [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]
+        >>> s.setZeroes([[1, 0, 2, 3]])
+        [[0, 0, 0, 0]]
+        >>> s.setZeroes([[0]])
+        [[0]]
+        >>> s.setZeroes([[]])
+        [[]]
         """
-        pass
+        m = len(matrix)
+        if m < 1:
+            return matrix
+
+        n = len(matrix[0])
+        if n < 1:
+            return matrix
+
+        # Check the most left and top positions have any zero
+        rowHasZero, colHasZero = False, False
+        for i in range(m):
+            if matrix[i][0] == 0:
+                rowHasZero = True
+                break
+
+        for j in range(n):
+            if matrix[0][j] == 0:
+                colHasZero = True
+                break
+
+        # Most left and top positions used for zero-flags
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0], matrix[0][j] = 0, 0
+
+        # Mark inner positions according to zero-flags
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+        if rowHasZero:
+            for i in range(m):
+                matrix[i][0] = 0
+
+        if colHasZero:
+            for j in range(n):
+                matrix[0][j] = 0
+
+        return matrix
 
 
 if __name__ == "__main__":
--- string/one_away.py
+++ string/one_away.py
@@ -1,17 +1,14 @@
 """
 1.5 One Away:
-There are three types of edits that can be performed on strings: insert a character, 
-remove a character, or replace a character. Given two strings, write a function to check ifthey are one edit (or zero edits) away. 
+There are three types of edits that can be performed on strings: insert a character,
+remove a character, or replace a character. Given two strings, write a function to check ifthey are one edit (or zero edits) away.
 
-EXAMPLE 
-pale, ple -> true 
-pales, pale -> true 
-pale, bale -> true 
-pale, bake -> false 
+EXAMPLE
+pale, ple -> true
+pales, pale -> true
+pale, bale -> true
+pale, bake -> false
 """
-
-from collections import defaultdict
-
 
 def solution(s1: str, s2: str) -> bool:
     """
@@ -19,38 +16,65 @@ def solution(s1: str, s2: str) -> bool:
     True
     >>> solution("pales", "pale")
     True
+    >>> solution("pale", "pales")
+    True
     >>> solution("pale", "bale")
     True
     >>> solution("pale", "bake")
     False
     >>> solution("pale", "bae")
     False
+    >>> solution("", "")
+    False
+    >>> solution("", "A")
+    True
+    >>> solution("A", "")
+    True
+    >>> solution("A", "A")
+    False
+    >>> solution("teacher", "detacher")
+    False
     """
     n1, n2 = len(s1), len(s2)
-    cnt_diff = 0
-    
+
     if n1 == n2:  # check replace
+        cnt_diff = 0
         for i in range(n1):
             if s1[i] != s2[i]:
                 cnt_diff += 1
-                
-    else:  # check insert / remove    
-        checker = defaultdict(lambda : 0)
-    
-        for ch in s1:
-            checker[ch] += 1
-        
-        for ch in s2:
-            checker[ch] -= 1
- 
-        cnt_diff = sum(map(abs, checker.values()))
 
-    if cnt_diff > 1:
-        return False
-           
-    return True
-    
-    
+        if cnt_diff == 1:
+            return True
+
+    elif n1 + 1 == n2:  # check insert
+        ins_pos = 0
+        for i in range(n1):
+            if s1[i] != s2[i]:
+                ins_pos = i
+                break
+        else:
+            return True
+
+        if (s1[:ins_pos] == s2[:ins_pos] and
+            s1[ins_pos:] == s2[ins_pos+1:]):
+            return True
+
+    elif n1 == n2 + 1:  # check remove
+        rem_pos = 0
+        for i in range(n2):
+            if s1[i] != s2[i]:
+                rem_pos = i
+                break
+        else:
+            return True
+
+        if (s2[:rem_pos] == s1[:rem_pos] and
+            s2[rem_pos:] == s1[rem_pos+1:]):
+            return True
+
+    return False
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
