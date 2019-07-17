a861ce8ace5e0fd9a99c6d21080652aed6c4bc00
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2019-07-17 22:05:43 +0900

    Add next permutation solution

--- array/next_permutation.py
+++ array/next_permutation.py
@@ -21,19 +21,71 @@ its corresponding outputs are in the right-hand column.
 
 
 class Solution(object):
-    def nextPermutation(self, nums):
-        """
+    def nextPermutation2(self, nums):
+        """ O(N), O(1)
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
-        >>> fn = Solution().nextPermutation
+        >>> fn = Solution().nextPermutation2
         >>> fn([1, 2, 3])
         [1, 3, 2]
         >>> fn([3, 2, 1])
         [1, 2, 3]
         >>> fn([1, 1, 5])
         [1, 5, 1]
+        >>> fn([5, 4, 2, 3, 1])
+        [5, 4, 3, 1, 2]
+        >>> fn([5, 4, 2, 9, 9])
+        [5, 4, 9, 2, 9]
+        >>> fn([5, 4, 2, 9, 5, 4])
+        [5, 4, 4, 2, 5, 9]
         """
-        pass
+        asc, n = -1, len(nums)
+        for i in range(n-1):
+            if nums[i] < nums[i+1]:
+                asc = i
+        next_min = asc + 1
+        for i in range(asc+2, n):
+            if nums[asc] < nums[i] <= nums[next_min]:
+                next_min = i
+        if asc != -1:
+            nums[asc], nums[next_min] = nums[next_min], nums[asc]
+        start, end = asc + 1, n - 1
+        for i in range((end - start) // 2 + 1):
+            nums[start+i], nums[end-i] = nums[end-i], nums[start+i]
+
+        return nums
+
+    def nextPermutation1(self, nums):
+        """ O(NlogN), O(N)
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        >>> fn = Solution().nextPermutation1
+        >>> fn([1, 2, 3])
+        [1, 3, 2]
+        >>> fn([3, 2, 1])
+        [1, 2, 3]
+        >>> fn([1, 1, 5])
+        [1, 5, 1]
+        >>> fn([5, 4, 2, 3, 1])
+        [5, 4, 3, 1, 2]
+        >>> fn([5, 4, 2, 9, 9])
+        [5, 4, 9, 2, 9]
+        >>> fn([5, 4, 2, 9, 5, 4])
+        [5, 4, 4, 2, 5, 9]
+        """
+        n = len(nums)
+        asc = -1
+        for i in range(n-1):
+            if nums[i] < nums[i+1]:
+                asc = i
+        next_min = asc + 1
+        for i in range(asc+2, n):
+            if nums[asc] < nums[i] <= nums[next_min]:
+                next_min = i
+        nums[asc], nums[next_min] = nums[next_min], nums[asc]
+        nums[asc+1:] = sorted(nums[asc+1:])
+
+        return nums
 
 
 if __name__ == "__main__":
