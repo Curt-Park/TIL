6a569aa450b67527bd0717f31f6f59cf4a6c6130
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2019-07-22 00:30:05 +0900

    Add trapping rain water problem

---
+++ array/trapping_rain_water.py
@@ -0,0 +1,33 @@
+"""
+https://leetcode.com/problems/trapping-rain-water/
+
+42. Trapping Rain Water
+Hard
+Given n non-negative integers representing an elevation map where the width of
+each bar is 1, compute how much water it is able to trap after raining.
+
+https://assets.leetcode.com/uploads/2018/10/22/rainwatertrap.png
+
+The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
+In this case, 6 units of rain water (blue section) are being trapped.
+Thanks Marcos for contributing this image!
+
+Example:
+
+Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+"""
+
+
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        pass
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
