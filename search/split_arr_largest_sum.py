17fb0f98136bc6251836e05a075fa5f6e0b708c3
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2019-10-05 13:08:26 +0900

    Add enhancement

--- search/split_arr_largest_sum.py
+++ search/split_arr_largest_sum.py
@@ -44,6 +44,7 @@ class Solution:
         while l <= r:
             mid = (l + r) // 2
             pieces = split(mid)
+            if pieces == mid: return mid
             if pieces > m: l = mid + 1
             else: r = mid - 1
         return l
