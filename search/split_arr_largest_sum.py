ea0a67e1d016ef987f2becd32e243b35d586d6db
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2019-10-05 14:07:36 +0900

    Remove unnecessary enhancement

--- search/split_arr_largest_sum.py
+++ search/split_arr_largest_sum.py
@@ -44,7 +44,6 @@ class Solution:
         while l <= r:
             mid = (l + r) // 2
             pieces = split(mid)
-            if pieces == mid: return mid
             if pieces > m: l = mid + 1
             else: r = mid - 1
         return l
