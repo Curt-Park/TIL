db0201f22df4f490ec8087812ea9dab9dde98dc6
Jinwoo Park <www.jwpark.co.kr@gmail.com>
2022-01-22 14:19:35 +0900

    Add producer and consumer in go

---
+++ go/concurrency/producerconsumer.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var msgs = make(chan int)
+var done = make(chan bool)
+
+func main() {
+	go produce(5)
+	go consume()
+	<-done
+	fmt.Println("Done")
+}
+
+func produce(n int) {
+	for i := 0; i < n; i++ {
+		time.Sleep(time.Second) // production time
+		msgs <- i // wait until consumed
+		fmt.Println("Produced", i)
+	}
+	close(msgs)  // channel must be closed by producer
+}
+
+func consume() {
+	for msg := range msgs {
+		fmt.Println("Consumed", msg)
+	}
+	done <- true // consumed all msgs
+}
