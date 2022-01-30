32b88c34a99a67a82bc99f13bcdab9dbf5828568
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2022-01-30 22:41:10 +0900

    Add restore sequence concurrency pattern in go

---
+++ go/concurrency/restore.go
@@ -0,0 +1,76 @@
+// Restore Sequence
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Message struct {
+	str  string
+	wait chan bool
+}
+
+func fanIn(inputs ...chan Message) chan Message {
+	c := make(chan Message)
+	for i := range inputs {
+		input := inputs[i]
+		go func() {
+			for {
+				c <- <-input
+			}
+		}()
+	}
+	return c
+}
+
+// the boring function return a channel to communicate with it.
+func boring(msg string) chan Message {
+	c := make(chan Message)
+	waitForIt := make(chan bool)
+	go func() {
+		for i := 0; ; i++ {
+			c <- Message{
+				str:  fmt.Sprintf("%s %d", msg, i),
+				wait: waitForIt,
+			}
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+
+			// every time the goroutine send message.
+			// This code waits until the value to be received.
+			<-waitForIt
+		}
+	}()
+
+	return c // return a channel to caller.
+}
+
+func main() {
+	// merge 2 channels into 1 channel
+	c := fanIn(boring("Joe"), boring("Ahn"))
+
+	for i := 0; i < 5; i++ {
+		msg1 := <-c // wait to receive a message
+		fmt.Println(msg1.str)
+		msg2 := <-c
+		fmt.Println(msg2.str)
+
+		// each go routine have to wait
+		msg1.wait <- true
+		msg2.wait <- true
+	}
+	fmt.Println("You're both boring. I'm leaving.")
+}
+
+// main: goroutine                                          boring: goroutine
+//    |                                                           |
+//    |                                                           |
+// wait for receiving msg from channel c                    c <- Message{} // send message
+//   <-c                                                          |
+//    |                                                           |
+//    |                                                     <-waitForIt // wait for wake up signal
+// send value to channel                                          |
+// hey, boring. You can send next value to me                     |
+//   wait <-true                                                  |
+///                            REPEAT THE PROCESS
