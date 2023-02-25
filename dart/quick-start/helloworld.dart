a71f9cbd871457c705806ea6e70992076e9a0869
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2023-02-25 11:52:23 +0900

    Add dart and flutter

---
+++ dart/quick-start/helloworld.dart
@@ -0,0 +1,28 @@
+// import 'dart:io';
+
+main() {
+  // hell world
+  print("Hello World");
+  print("From Dart");
+  // print("Hello ${stdin.readLineSync()}");
+
+  // Variables
+  int myFirstDartVariable = 5;
+  print(myFirstDartVariable);
+
+  // Types
+  num firstNumber = 5;
+  num secondNumber = 5.1;
+  num thirdNumber = firstNumber;
+  print(firstNumber);
+  print(secondNumber);
+  print(thirdNumber);
+  int simpleInteger = 1;
+  int hex = 0xDA34F;
+  int integer = simpleInteger;
+  print(simpleInteger);
+  print(hex);
+  print(integer);
+  String s1 = "A String";
+  print(s1);
+}
