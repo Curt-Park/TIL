8c95afa978e738654cd013bc1da1ac0665e3475b
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2023-06-18 13:21:22 +0900

    Solve leetcode 084

--- algorithm/leetcode_top_100_liked/084_largest_rectangle_in_histogram.go
+++ algorithm/leetcode_top_100_liked/084_largest_rectangle_in_histogram.go
@@ -1,7 +1,48 @@
 package main
 
+func largestRectangleArea1(heights []int) int {
+	area := make([]int, len(heights))
+	getArea(&heights, &area)
+	for i := 0; i < len(heights)/2; i++ {
+		heights[i], heights[len(heights)-i-1] = heights[len(heights)-i-1], heights[i]
+		area[i], area[len(heights)-i-1] = area[len(heights)-i-1], area[i]
+	}
+	getArea(&heights, &area)
+	maxArea := 0
+	for i := 0; i < len(heights); i++ {
+		area := area[i] - heights[i]
+		if maxArea < area {
+			maxArea = area
+		}
+	}
+	return maxArea
+}
+
+func getArea(heights *[]int, area *[]int) {
+	stack := []int{}
+	for i, h := range *heights {
+		if len(stack) > 0 {
+			loc := stack[len(stack)-1]
+			for len(stack) > 0 && (*heights)[stack[len(stack)-1]] > h {
+				idx := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				(*area)[idx] += (*heights)[idx] * (loc - idx + 1)
+			}
+		}
+		stack = append(stack, i)
+	}
+	if len(stack) > 0 {
+		loc := stack[len(stack)-1]
+		for len(stack) > 0 {
+			idx := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			(*area)[idx] += (*heights)[idx] * (loc - idx + 1)
+		}
+	}
+}
+
 // TLE
-func largestRectangleArea(heights []int) int {
+func largestRectangleArea2(heights []int) int {
 	ans := 0
 	for i, h := range heights {
 		l, r := i, i
