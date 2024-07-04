e585cf0f6c3bd395c0385f24e319630c3d179432
curt.park <curt.park@snowcorp.com>
2024-07-04 12:09:01 +0900

    Refactor

--- openai/crawl_web.py
+++ openai/crawl_web.py
@@ -11,7 +11,6 @@ import re
 import urllib.request
 from collections import deque
 from html.parser import HTMLParser
-from typing import Optional
 from urllib.parse import urlparse
 
 import requests
@@ -28,12 +27,14 @@ class HyperlinkParser(HTMLParser):
         self.hyperlinks: list[str] = []
 
     def handle_starttag(self, tag: str, attrs: list[tuple[str, str]]) -> None:
+        """Override handle_starttag."""
         attrs = dict(attrs)
         if tag == "a" and "href" in attrs:
             self.hyperlinks.append(attrs["href"])
 
 
 def get_hyperlinks(url: str) -> list[str]:
+    """Get hyperlinks without duplicates."""
     try:
         with urllib.request.urlopen(url) as response:
             if not response.info().get("Content-Type").startswith("text/html"):
@@ -52,33 +53,33 @@ def get_hyperlinks(url: str) -> list[str]:
 def get_domain_hyperlinks(
     local_domain: str, url: str, http_url_pattern: str = r"^http[s]*://.+"
 ) -> list[str]:
+    """Get valid hyperlinks with the same local domain."""
     clean_links = []
-    for link in set(get_hyperlinks(url)):
-        clean_link: Optional[str] = None
+    for link in get_hyperlinks(url):
+        # Skip.
+        url_pattern_matched = re.search(http_url_pattern, link) is not None
+        if url_pattern_matched and urlparse(link).netloc != local_domain:
+            logging.info("skip %s due to unmatched local domain", link)
+            continue
+        if not url_pattern_matched and link.startswith("#") or link.startswith("mailto:"):
+            logging.info("skip %s due to invalid hyperlink", link)
+            continue
 
-        # If the link is a URL, check if it is within the same domain
-        if re.search(http_url_pattern, link):
-            # Parse the URL and check if the domain is the same
-            if urlparse(link).netloc == local_domain:
-                clean_link = link
-        # If the link is not a URL, check if it is a relative link
-        else:
-            if link.startswith("/"):
-                link = link[1:]
-            elif link.startswith("#") or link.startswith("mailto:"):
-                continue
-            clean_link = "https://" + local_domain + "/" + link
+        # Format.
+        clean_link = link
+        clean_link = clean_link if not link.startswith("/") else clean_link[1:]
+        clean_link = clean_link if not clean_link.endswith("/") else clean_link[:-1]
+        clean_link = clean_link if url_pattern_matched else f"https://{local_domain}/{clean_link}"
 
-        if clean_link:
-            if clean_link.endswith("/"):
-                clean_link = clean_link[:-1]
-            clean_links.append(clean_link)
+        # Append.
+        clean_links.append(clean_link)
 
     # Return the list of hyperlinks that are within the same domain
     return list(set(clean_links))
 
 
 def save_html_as_txt(out_dir: str, url: str) -> None:
+    """Save html as {out_dir}/{u_r_l}.txt."""
     txt_name = os.path.join(out_dir, f"{url[8:].replace('/', '_')}.txt")
     with open(txt_name, "w", encoding="utf-8") as f:
         soup = BeautifulSoup(requests.get(url).text, "html.parser")
@@ -89,6 +90,7 @@ def save_html_as_txt(out_dir: str, url: str) -> None:
 
 
 def crawl(url: str, max_depth: int) -> None:
+    """Crawl web-pages that doesn't exceed the max_depth."""
     local_domain = urlparse(url).netloc
 
     out_dir = os.path.join("parsed", local_domain)
@@ -99,7 +101,6 @@ def crawl(url: str, max_depth: int) -> None:
     while queue:
         url, depth = queue.pop()
         logging.info("process %s", url)
-
         save_html_as_txt(out_dir, url)
 
         for link in get_domain_hyperlinks(local_domain, url):
@@ -110,10 +111,12 @@ def crawl(url: str, max_depth: int) -> None:
             seen.add(link)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--url", type=str, required=True)
-parser.add_argument("--max-depth", type=int, default=3)
-args = parser.parse_args()
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", type=str, required=True)
+    parser.add_argument("--max-depth", type=int, default=3)
+    args = parser.parse_args()
+    crawl(args.url, args.max_depth)
 
 
-crawl(args.url, args.max_depth)
+main()
