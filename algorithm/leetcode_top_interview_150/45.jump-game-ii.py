ddce48ee52ecd0ed24990ea3d93c858b793777c5
curt-park <www.jwpark.co.kr@gmail.com>
2024-11-22 09:09:43 +0900

    Solve 1 leetcode problem

--- algorithm/leetcode_top_interview_150/45.jump-game-ii.py
+++ algorithm/leetcode_top_interview_150/45.jump-game-ii.py
@@ -6,6 +6,18 @@
 
 # @lc code=start
 class Solution:
+    # O(N^2)
+    def jump_0(self, nums: List[int]) -> int:
+        n_jumps, N = [float("inf")] * len(nums), len(nums)
+        n_jumps[-1] = 0
+        for i in reversed(range(N-1)):
+            if not nums[i]:
+                continue
+            lookup = n_jumps[i+1:i+nums[i]+1]
+            n_jumps[i] = min(lookup) + 1
+        return n_jumps[0]
+
     def jump(self, nums: List[int]) -> int:
+        pass
 # @lc code=end
 
