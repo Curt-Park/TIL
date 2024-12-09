0fe4ff3c377f693d4ea34098623ce2494cb11164
curt-park <www.jwpark.co.kr@gmail.com>
2024-12-10 00:12:36 +0900

    Make more progress of advert of code 24 day 5 part2

--- algorithm/advent_of_code_2024/day5/sol.py
+++ algorithm/advent_of_code_2024/day5/sol.py
@@ -1,24 +1,43 @@
+import sys
 from collections import defaultdict
 
 
+filename = sys.argv[1] if len(sys.argv) > 1 else "test.txt"
+
 g = defaultdict(set)
+v = set()
 pages = []
 
-with open("test.txt", "r") as f:
-    for line in f.readlines():
-        split = line.split("|")
-        if len(split) > 1:
-            g[int(split[0])].add(int(split[1]))
-        split = line.split(",")
-        if len(split) > 1:
-            p = [int(n) for n in split]
-            pages.append(p[::-1])
+with open(filename, "r") as f:
+    edges, queries = f.read().split("\n\n")
+    for x, y in [edge.split("|") for edge in edges.split()]:
+        g[int(x)].add(int(y))
+    for query in queries.split():
+        p = []
+        for n in query.split(","):
+            p.append(int(n))
+            v.add(int(n))
+        pages.append(p)
+
+
+def get_reachable(graph, start) -> set[int]:
+    def dfs(node, visited):
+        if node in visited:
+            return
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(neighbor, visited)
+
+    visited = set()
+    dfs(start, visited)
+    visited.remove(start)
+    return visited
 
 
 def validate(g, page) -> bool:
-    for i in range(len(page) - 1):
-        for j in range(i+1, len(page)):
-            if page[j] in g[page[i]]:
+    for i, x in enumerate(page):
+        for j, y in enumerate(page):
+            if i < j and x in g[y]:
                 return False
     return True
 
@@ -26,21 +45,37 @@ def validate(g, page) -> bool:
 def print_queue_0(g, pages) -> int:
     s = 0
     for page in pages:
-        if not validate(g, page):
-            continue
-        mid = page[len(page) // 2]
-        s += mid
+        if validate(g, page):
+            mid = page[len(page) // 2]
+            s += mid
     return s
 
 
 def print_queue_1(g, pages) -> int:
+    reachable = defaultdict(set)
+    for node in v:
+        reachable[node] = get_reachable(g, node)
+
     s = 0
     for page in pages:
         if validate(g, page):
             continue
-        print("invalid:", page[::-1])
-        # sort... how?
-        mid = page[len(page) // 2]
+        # sort
+        sorted_page = []
+        for node in page:
+            sorted_page.append(node)
+            for i in range(len(sorted_page)-1, 0, -1):
+                if sorted_page[i] in reachable[sorted_page[i-1]]:
+                    break
+                sorted_page[i], sorted_page[i-1] = sorted_page[i-1], sorted_page[i]
+
+        # validate
+        for i, x in enumerate(sorted_page):
+            for j, y in enumerate(sorted_page):
+                if i < j and y not in reachable[x]:
+                    print(sorted_page)
+
+        mid = sorted_page[len(sorted_page) // 2]
         s += mid
     return s
 
